0e6a679fd01b0750a962411ba625ef6b017ec10f
himarg2274 <himarosegeorge@gmail.com>
2024-10-29 19:46:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JavaScript loaded!");
+    // Placeholder for future interactive functions
+  });
+  // script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Product page loaded!");
+    // Future JavaScript to fetch product data and load into product list
+  });
+  
+  
